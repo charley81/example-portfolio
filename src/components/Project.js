12dd502fea3861d2104b1936f860a53c0076fbd7
ChrisKnowsDev <chrisharley81@gmail.com>
2021-03-02 18:34:13 -0500

    added projects section to home page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,32 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Image from "gatsby-image"
-import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
+import React from 'react'
+import PropTypes from 'prop-types'
+import Image from 'gatsby-image'
+import { FaGithubSquare, FaShareSquare } from 'react-icons/fa'
+
+const Project = ({ description, title, github, stack, url, image, index }) => {
+  return (
+    <article className="project">
+      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      <div className="project-info">
+        <span className="project-number">0{index + 1}</span>
+        <h3>{title}</h3>
+        <p className="project-description">{description}</p>
+        <div className="project-stack">
+          {stack.map(item => (
+            <span key={item.id}>{item.title}</span>
+          ))}
+        </div>
+        <div className="project-links">
+          <a href={github} className="project-icon">
+            <FaGithubSquare />
+          </a>
+          <a href={url} className="project-icon">
+            <FaShareSquare />
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 Project.propTypes = {}
