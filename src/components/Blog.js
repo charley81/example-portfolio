ae04b68214c3d8d37d4b69468655f92d756b4a3f
ChrisKnowsDev <chrisharley81@gmail.com>
2021-03-03 11:12:05 -0500

    showing solo blog on home page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,24 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Image from "gatsby-image"
-import { Link } from "gatsby"
-const Blog = () => {
-  return <h2>blog component</h2>
+import React from 'react'
+import PropTypes from 'prop-types'
+import Image from 'gatsby-image'
+import { Link } from 'gatsby'
+
+const Blog = ({ id, title, slug, desc, image, date, category }) => {
+  return (
+    <Link to={`/blog/${slug}`} key={id} className="blog">
+      <article>
+        <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        <div className="blog-card">
+          <h4>{title}</h4>
+          <p>{desc}</p>
+          <div className="blog-footer">
+            <p>{category}</p>
+            <p>{date}</p>
+          </div>
+        </div>
+      </article>
+    </Link>
+  )
 }
 
 Blog.propTypes = {}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,37 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
+import React from 'react'
+import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+import Blogs from '../components/Blogs'
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({ data }) => {
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs title="blog" />
+      </section>
+    </Layout>
+  )
 }
-
 export default Blog
+
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        slug
+        desc
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
